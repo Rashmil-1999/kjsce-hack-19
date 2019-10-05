cd48e1694e6ec14efd3dad8420faaaed0bec746d
Rashmil Panchani <32737711+Rashmil-1999@users.noreply.github.com>
2019-10-05 22:33:18 +0530

    added auth

--- src/containers/Auth/SignUpCard.jsx
+++ src/containers/Auth/SignUpCard.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
-import { Card, CardHeader, CardBody, Input, Button } from 'reactstrap';
-import { useInput } from '../../util';
+import { Link } from "react-router-dom";
+import { Card, CardHeader, CardBody, Input, Button } from "reactstrap";
+import { useInput } from "../../util";
 
-const SignUpCard: React.FC = () => {
+const SignUpCard = () => {
   const nameInput = useInput(null);
   const phoneNumberInput = useInput(null);
   const specialityInput = useInput(null);
@@ -16,44 +16,58 @@ const SignUpCard: React.FC = () => {
       <CardHeader className="text-center border-0">
         <h4 className="m-0">Sign Up</h4>
       </CardHeader>
-      <CardBody tag="form" onSubmit={() => alert("Hi")} className="d-flex flex-column">
+      <CardBody
+        tag="form"
+        onSubmit={() => alert("Hi")}
+        className="d-flex flex-column"
+      >
         <Input
           className="border-radius-50 my-1"
           placeholder="Enter your name"
-          {...nameInput as any}
+          {...nameInput}
         />
 
         <Input
           className="border-radius-50 my-1"
           placeholder="Enter Phone Number"
           type="tel"
-          {...phoneNumberInput as any}
+          {...phoneNumberInput}
         />
 
         <Input
           className="border-radius-50 my-1"
           placeholder="Enter your speciality"
-          {...specialityInput as any}
+          {...specialityInput}
         />
 
         <Input
           className="border-radius-50 my-1"
           placeholder="Enter the year you started your practice"
           type="number"
-          {...experienceInput as any}
+          {...experienceInput}
         />
 
         <Input
           className="border-radius-50 my-1"
           placeholder="Enter your clinic address"
-          {...addressInput as any}
+          {...addressInput}
         />
 
-        <Button color="danger" type="submit" className="rounded-pill my-1">Sign Up</Button>
-        <Button tag={Link} to="/login" outline color="warning" className="rounded-pill my-1">Log In</Button>
+        <Button color="danger" type="submit" className="rounded-pill my-1">
+          Sign Up
+        </Button>
+        <Button
+          tag={Link}
+          to="/login"
+          outline
+          color="warning"
+          className="rounded-pill my-1"
+        >
+          Log In
+        </Button>
       </CardBody>
     </Card>
   );
-}
+};
 
 export default SignUpCard;
