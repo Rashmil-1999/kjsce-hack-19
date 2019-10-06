0c1e83349fd48415f77aef3d4e9175b6692b129b
Vikrant Gajria <vikrantgajria@gmail.com>
2019-10-06 11:39:25 +0530

    LALALALAL

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import {
   NavLink
@@ -7,12 +7,33 @@ import {
 import {
   Navbar,
   NavbarBrand,
+  Nav,
+  NavItem,
 } from 'reactstrap';
 
+import { useFirebaseUser } from '../../util/firebase/extras';
+
 const MyNavbar: React.FC = () => {
+  const user = useFirebaseUser();
+
+  const name = useMemo(() => {
+    if(user) {
+      return user.phoneNumber;
+    } else {
+      return "Not logged in";
+    }
+  }, [user]);
+
   return (
-    <Navbar color="primary" dark>
+    <Navbar dark style={{
+      backgroundImage: "linear-gradient(315deg, #ee9617 0%, #fe5858 74%)",
+    }}>
       <NavbarBrand tag={NavLink} to='/'>Doktor</NavbarBrand>
+      <Nav>
+        <NavItem className="ml-auto">
+          {name}
+        </NavItem>
+      </Nav>
     </Navbar>
   );
 };
--- src/components/Patient/Biodata.tsx
+++ src/components/Patient/Biodata.tsx
@@ -11,6 +11,7 @@ import {
   Col,
 } from 'reactstrap';
 
+import styled from 'styled-components';
 import _ from 'lodash';
 
 type UserDocType = {
@@ -22,6 +23,12 @@ type UserDocType = {
   bloodGroup: string,
   sex: string,
 };
+
+const GradButton = styled(Button)`
+  background-color: #7ee8fa;
+  background-image: linear-gradient(315deg, #7ee8fa 0%, #80ff72 74%);
+  border: none;
+`;
 
 const UserInfo: React.FC<UserDocType & { onFillClick: () => void }> = (props) => {
   const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -59,7 +66,7 @@ const UserInfo: React.FC<UserDocType & { onFillClick: () => void }> = (props) =>
       </CardBody>
 
       <CardFooter tag="form" className="d-flex flex-column px-5">
-        <Button type="submit" color="info" size="lg" onClick={onClick}>Fill Prescription</Button>
+        <GradButton type="submit" color="info" size="lg" onClick={onClick}>Fill Prescription</GradButton>
       </CardFooter>
     </Card>
   );
